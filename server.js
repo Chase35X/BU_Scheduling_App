e2c1a7d5f1a495c2f68bfc11c4b4fc99a75b5669
Krish Asija <krishasija@Krishs-MacBook-Pro.local>
2024-01-11 20:05:50 -0500

    Hosting all html pages correctly through express server

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const path = require('path')
+const fs = require('fs')
 
+//connect db
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 
 const db = mongoose.connection
@@ -13,13 +15,35 @@ db.once('open', () => console.log('Connected to database'))
 
 app.use(express.json())
 
+//reservation middleware
 const reservationsRouter = require('./routes/reservations')
 app.use('/reservations', reservationsRouter)
 
-app.use(express.static(path.join(__dirname, 'home_page')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home_page', 'index.html'))
-})
+//
+const publicPath = path.join(__dirname, 'public')
+app.use(express.static(publicPath));
+
+const folders = fs.readdirSync(path.join(publicPath, 'BU_Scheduling_App'));
+
+folders.forEach((folder) => {
+    console.log(folder)
+    const folderPath = path.join(publicPath,'BU_Scheduling_App', folder)
+    const stats = fs.statSync(folderPath)
+
+    if (stats.isDirectory()) {
+        app.use(`/${folder}`, express.static(folderPath))
+        app.get(`/${folder}`, (req, res) => {
+            res.sendFile(path.join(folderPath, 'index.html'))
+        });
+    }
+});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(`${publicPath}/home_page/index.html`)
+// })
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'home_page', 'index.html'))
+// })
 
 const PORT = 3000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
